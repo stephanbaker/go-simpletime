ee2c76790eab0568256536367a3f7949c1e62d4e
Stephan Baker <stephan@forestgiant.com>
2015-08-04 03:09:46 -0400

    Adding a basic extension of the go standard library time package which provides some helper functions for obtaining the beginning/end of a time period as well as the next occurrence of a given weekday.

---
+++ simpletime.go
@@ -0,0 +1,98 @@
+package simpletime
+
+import (
+	"time"
+)
+
+type SimpleTime struct {
+	time.Time
+}
+
+func NewSimpleTime(t time.Time) *SimpleTime {
+	return &SimpleTime{t}
+}
+
+func (t *SimpleTime) BeginningOfYear() time.Time {
+	return time.Date(t.Year(), 1, 1, 0, 0, 0, 0, t.Location())
+}
+
+func (t *SimpleTime) BeginningOfMonth() time.Time {
+	return time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, t.Location())
+}
+
+func (t *SimpleTime) BeginningOfDay() time.Time {
+	return t.Truncate(time.Hour * 24)
+}
+
+func (t *SimpleTime) BeginningOfHour() time.Time {
+	return t.Truncate(time.Hour)
+}
+
+func (t *SimpleTime) BeginningOfMinute() time.Time {
+	return t.Truncate(time.Minute)
+}
+
+func (t *SimpleTime) BeginningOfSecond() time.Time {
+	return t.Truncate(time.Second)
+}
+
+func (t *SimpleTime) EndOfYear() time.Time {
+	return t.BeginningOfYear().AddDate(1, 0, 0).Add(-time.Nanosecond)
+}
+
+func (t *SimpleTime) EndOfMonth() time.Time {
+	return t.BeginningOfMonth().AddDate(0, 1, 0).Add(-time.Nanosecond)
+}
+
+func (t *SimpleTime) EndOfDay() time.Time {
+	return t.BeginningOfDay().AddDate(0, 0, 1).Add(-time.Nanosecond)
+}
+
+func (t *SimpleTime) EndOfHour() time.Time {
+	return t.BeginningOfHour().Add(time.Hour - time.Nanosecond)
+}
+
+func (t *SimpleTime) EndOfMinute() time.Time {
+	return t.BeginningOfMinute().Add(time.Minute - time.Nanosecond)
+}
+
+func (t *SimpleTime) EndOfSecond() time.Time {
+	return t.BeginningOfSecond().Add(time.Second - time.Nanosecond)
+}
+
+func (t *SimpleTime) NextOccurrenceOfWeekday(weekday time.Weekday) time.Time {
+	days := (int(weekday) - int(t.Weekday()) + 7) % 7
+	next := NewSimpleTime(t.AddDate(0, 0, days))
+	if days > 0 {
+		return next.BeginningOfDay()
+	}
+	return next.Time
+}
+
+func (t *SimpleTime) NextSunday() time.Time {
+	return t.NextOccurrenceOfWeekday(time.Weekday(0))
+}
+
+func (t *SimpleTime) NextMonday() time.Time {
+	return t.NextOccurrenceOfWeekday(time.Weekday(1))
+}
+
+func (t *SimpleTime) NextTuesday() time.Time {
+	return t.NextOccurrenceOfWeekday(time.Weekday(2))
+}
+
+func (t *SimpleTime) NextWednesday() time.Time {
+	return t.NextOccurrenceOfWeekday(time.Weekday(3))
+}
+
+func (t *SimpleTime) NextThursday() time.Time {
+	return t.NextOccurrenceOfWeekday(time.Weekday(4))
+}
+
+func (t *SimpleTime) NextFriday() time.Time {
+	return t.NextOccurrenceOfWeekday(time.Weekday(5))
+}
+
+func (t *SimpleTime) NextSaturday() time.Time {
+	return t.NextOccurrenceOfWeekday(time.Weekday(6))
+}
---
+++ simpletime_test.go
@@ -0,0 +1,213 @@
+package simpletime
+
+import (
+	"testing"
+	"time"
+)
+
+func TestBeginningOfYear(t *testing.T) {
+	input := time.Date(2001, 2, 3, 4, 5, 6, 7, time.UTC)
+	expected := time.Date(input.Year(), 1, 1, 0, 0, 0, 0, input.Location())
+
+	result := NewSimpleTime(input).BeginningOfYear()
+	if result != expected {
+		t.Errorf("TestBeginningOfYear Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+}
+
+func TestBeginningOfMonth(t *testing.T) {
+	input := time.Date(2001, 2, 3, 4, 5, 6, 7, time.UTC)
+	expected := time.Date(input.Year(), input.Month(), 1, 0, 0, 0, 0, input.Location())
+
+	result := NewSimpleTime(input).BeginningOfMonth()
+	if result != expected {
+		t.Errorf("TestBeginningOfMonth Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+}
+
+func TestBeginningOfDay(t *testing.T) {
+	input := time.Date(2001, 2, 3, 4, 5, 6, 7, time.UTC)
+	expected := time.Date(input.Year(), input.Month(), input.Day(), 0, 0, 0, 0, input.Location())
+
+	result := NewSimpleTime(input).BeginningOfDay()
+	if result != expected {
+		t.Errorf("TestBeginningOfDay Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+}
+
+func TestBeginningOfHour(t *testing.T) {
+	input := time.Date(2001, 2, 3, 4, 5, 6, 7, time.UTC)
+	expected := time.Date(input.Year(), input.Month(), input.Day(), input.Hour(), 0, 0, 0, input.Location())
+
+	result := NewSimpleTime(input).BeginningOfHour()
+	if result != expected {
+		t.Errorf("TestBeginningOfHour Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+}
+
+func TestBeginningOfMinute(t *testing.T) {
+	input := time.Date(2001, 2, 3, 4, 5, 6, 7, time.UTC)
+	expected := time.Date(input.Year(), input.Month(), input.Day(), input.Hour(), input.Minute(), 0, 0, input.Location())
+
+	result := NewSimpleTime(input).BeginningOfMinute()
+	if result != expected {
+		t.Errorf("TestBeginningOfMinute Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+}
+
+func TestBeginningOfSecond(t *testing.T) {
+	input := time.Date(2001, 2, 3, 4, 5, 6, 7, time.UTC)
+	expected := time.Date(input.Year(), input.Month(), input.Day(), input.Hour(), input.Minute(), input.Second(), 0, input.Location())
+
+	result := NewSimpleTime(input).BeginningOfSecond()
+	if result != expected {
+		t.Errorf("TestBeginningOfSecond Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+}
+
+func TestEndOfYear(t *testing.T) {
+	input := time.Date(2001, 2, 3, 4, 5, 6, 7, time.UTC)
+	expected := time.Date(input.Year(), 12, 31, 23, 59, 59, 999999999, input.Location())
+
+	result := NewSimpleTime(input).EndOfYear()
+	if result != expected {
+		t.Errorf("TestEndOfYear Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+}
+
+func TestEndOfMonth(t *testing.T) {
+	input := time.Date(2001, 2, 3, 4, 5, 6, 7, time.UTC)
+	expected := time.Date(input.Year(), input.Month(), 28, 23, 59, 59, 999999999, input.Location())
+
+	result := NewSimpleTime(input).EndOfMonth()
+	if result != expected {
+		t.Errorf("TestEndOfMonth Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+}
+
+func TestEndOfDay(t *testing.T) {
+	input := time.Date(2001, 2, 3, 4, 5, 6, 7, time.UTC)
+	expected := time.Date(input.Year(), input.Month(), input.Day(), 23, 59, 59, 999999999, input.Location())
+
+	result := NewSimpleTime(input).EndOfDay()
+	if result != expected {
+		t.Errorf("TestEndOfDay Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+}
+
+func TestEndOfHour(t *testing.T) {
+	input := time.Date(2001, 2, 3, 4, 5, 6, 7, time.UTC)
+	expected := time.Date(input.Year(), input.Month(), input.Day(), input.Hour(), 59, 59, 999999999, input.Location())
+
+	result := NewSimpleTime(input).EndOfHour()
+	if result != expected {
+		t.Errorf("TestEndOfHour Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+}
+
+func TestEndOfMinute(t *testing.T) {
+	input := time.Date(2001, 2, 3, 4, 5, 6, 7, time.UTC)
+	expected := time.Date(input.Year(), input.Month(), input.Day(), input.Hour(), input.Minute(), 59, 999999999, input.Location())
+
+	result := NewSimpleTime(input).EndOfMinute()
+	if result != expected {
+		t.Errorf("TestEndOfMinute Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+}
+
+func TestEndOfSecond(t *testing.T) {
+	input := time.Date(2001, 2, 3, 4, 5, 6, 7, time.UTC)
+	expected := time.Date(input.Year(), input.Month(), input.Day(), input.Hour(), input.Minute(), input.Second(), 999999999, input.Location())
+
+	result := NewSimpleTime(input).EndOfSecond()
+	if result != expected {
+		t.Errorf("TestEndOfMinute Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+}
+
+func TestNextOccurrenceOfWeekday(t *testing.T) {
+	input := time.Date(2001, 2, 3, 4, 5, 6, 7, time.UTC)
+	expected := input
+
+	result := NewSimpleTime(input).NextOccurrenceOfWeekday(input.Weekday())
+	if result != expected {
+		t.Errorf("TestNextOccurrenceOfWeekday Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+
+	expected = time.Date(2001, 2, 9, 0, 0, 0, 0, time.UTC)
+
+	result = NewSimpleTime(input).NextOccurrenceOfWeekday(time.Weekday(5))
+	if result != expected {
+		t.Errorf("TestNextOccurrenceOfWeekday Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+}
+
+func TestNextSunday(t *testing.T) {
+	input := time.Date(2001, 2, 3, 4, 5, 6, 7, time.UTC)
+	expected := time.Date(2001, 2, 4, 0, 0, 0, 0, time.UTC)
+
+	result := NewSimpleTime(input).NextSunday()
+	if result != expected {
+		t.Errorf("TestNextSunday Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+}
+
+func TestNextMonday(t *testing.T) {
+	input := time.Date(2001, 2, 3, 4, 5, 6, 7, time.UTC)
+	expected := time.Date(2001, 2, 5, 0, 0, 0, 0, time.UTC)
+
+	result := NewSimpleTime(input).NextMonday()
+	if result != expected {
+		t.Errorf("TestNextMonday Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+}
+
+func TestNextTuesday(t *testing.T) {
+	input := time.Date(2001, 2, 3, 4, 5, 6, 7, time.UTC)
+	expected := time.Date(2001, 2, 6, 0, 0, 0, 0, time.UTC)
+
+	result := NewSimpleTime(input).NextTuesday()
+	if result != expected {
+		t.Errorf("TestNextTuesday Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+}
+
+func TestNextWednesday(t *testing.T) {
+	input := time.Date(2001, 2, 3, 4, 5, 6, 7, time.UTC)
+	expected := time.Date(2001, 2, 7, 0, 0, 0, 0, time.UTC)
+
+	result := NewSimpleTime(input).NextWednesday()
+	if result != expected {
+		t.Errorf("TestNextWednesday Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+}
+
+func TestNextThursday(t *testing.T) {
+	input := time.Date(2001, 2, 3, 4, 5, 6, 7, time.UTC)
+	expected := time.Date(2001, 2, 8, 0, 0, 0, 0, time.UTC)
+
+	result := NewSimpleTime(input).NextThursday()
+	if result != expected {
+		t.Errorf("TestNTestNextThursdayextSunday Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+}
+
+func TestNextFriday(t *testing.T) {
+	input := time.Date(2001, 2, 3, 4, 5, 6, 7, time.UTC)
+	expected := time.Date(2001, 2, 9, 0, 0, 0, 0, time.UTC)
+
+	result := NewSimpleTime(input).NextFriday()
+	if result != expected {
+		t.Errorf("TestNextFriday Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+}
+
+func TestNextSaturday(t *testing.T) {
+	input := time.Date(2001, 2, 3, 4, 5, 6, 7, time.UTC)
+	expected := input
+
+	result := NewSimpleTime(input).NextSaturday()
+	if result != expected {
+		t.Errorf("TestNextSaturday Failed:\nInput=(%v),\nExpected=(%v),\nResult=(%v)", input, expected, result)
+	}
+}
